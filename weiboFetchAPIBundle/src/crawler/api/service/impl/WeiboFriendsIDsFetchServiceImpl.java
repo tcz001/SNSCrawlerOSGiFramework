1412ca2a35a39408cc35b9a663655e10059c7e28
蒋颿 <tcz@Lenovo-PC.(none)>
2013-03-05 15:01:21 +0800

    JSON

--- weiboFetchAPIBundle/src/crawler/api/service/impl/WeiboFriendsIDsFetchServiceImpl.java
+++ weiboFetchAPIBundle/src/crawler/api/service/impl/WeiboFriendsIDsFetchServiceImpl.java
@@ -3,12 +3,13 @@ package crawler.api.service.impl;
 import org.scribe.model.OAuthRequest;
 import org.scribe.model.Response;
 import org.scribe.model.Verb;
+import net.sf.json.*;
 
 import crawler.api.service.fetch.FetchService;
 
 public class WeiboFriendsIDsFetchServiceImpl implements FetchService {
 	private static final String PROTECTED_RESOURCE_URL = "https://api.weibo.com/2/friendships/friends/ids.json";
-
+	JSONObject json;
 	OAuthimpl oAuthimpl;
 
 	@Override
@@ -29,8 +30,8 @@ public class WeiboFriendsIDsFetchServiceImpl implements FetchService {
 		System.out.println();
 		System.out.println(response.getCode());
 		System.out.println(response.getBody());
-
-		System.out.println();
+		json= JSONObject.fromObject(response.getBody());
+		System.out.println(json.toString());
 		System.out
 				.println("Thats it man! Go and build something awesome with Scribe! :)");
 
